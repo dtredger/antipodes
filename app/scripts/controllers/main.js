e3578ba5032ef35989f4db914cf7c58bce4335ed
dtredger <d.tredger@gmail.com>
2014-11-30 19:58:34 -0500

    use angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name antipodesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the antipodesApp
+ */
+angular.module('antipodesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
